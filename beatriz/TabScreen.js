8c1f9db63c7d4fcb5bf49efab68e3c141cfa0ccd
Beatriz L <158210831+limabea23@users.noreply.github.com>
2025-04-15 09:24:16 -0300

    feat: start first page

---
+++ beatriz/TabScreen.js
@@ -0,0 +1,10 @@
+import React from "react"; 
+import { SafeAreaView, View, Text, StyleSheet } from "react-native";
+
+export default function TabScreen() {
+    return (
+        <SafeAreaView styles={styles.container}>
+            <Text style={styles.title}>Harry Styles</Text>
+        </SafeAreaView>
+    )
+}
