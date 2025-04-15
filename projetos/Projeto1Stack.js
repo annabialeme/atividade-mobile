8d99f3fedc3df372eef4bdb22ef78e23940df1aa
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-04-15 10:28:00 -0300

    update

---
+++ projetos/Projeto1Stack.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../pages/HomeScreen";
+import ProfileScreen from "../pages/ProfileScreen";
+import dados from "../data/dados";
+
+const Stack = createNativeStackNavigator();
+
+export default function Projeto1Stack() {
+    const aluno = dados [0];
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Home">{(props) =>HomeScreen {...props} aluno={aluno} />}</Stack.Screen>
+            <Stack.Screen name="Profile">{(props) =>ProfileScreen {...props} aluno={aluno} />}</Stack.Screen>
+        </Stack.Navigator>
+    );
+}
