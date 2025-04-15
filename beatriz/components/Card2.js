8c16db19053fa70f6e8bb1a86d4fcaf714124eb0
Beatriz L <158210831+limabea23@users.noreply.github.com>
2025-04-15 11:45:26 -0300

    feat: add icon

--- beatriz/components/Card2.js
+++ beatriz/components/Card2.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { View, Image, Text, StyleSheet } from 'react-native';
 
+import AntDesign from '@expo/vector-icons/AntDesign';
+
 export default function Card2({ image, text }) {
     return (
         <View style={StyleSheet.card2}>
             <Image style={styles.image}>{image}</Image>
             <Text style={styles.text}>{text}</Text>
+            <AntDesign name="plus" size={24} color="black" />
         </View>
     );
 }
