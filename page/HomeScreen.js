c09b2310a9aa836ff5d366bdaf6eb955c0edcae1
maria parma <mariaparma.tds1.senai@gmail.com>
2025-04-15 09:09:13 -0300

    feat inicial code

---
+++ page/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StylesSheet } from "react-native";
+
+
+export default function Home() {  {navigation}} {
+    return (
+        <View style={StylesSheet.container}>
+        <Text style={style.text}>Maria Eduarda Parma</Text>
+        
+        </View>
+    )
+}
