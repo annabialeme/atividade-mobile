39cec224dbe39b94688f00e4f95ae1233bb6ea02
anna leme <162629023+annabialeme@users.noreply.github.com>
2025-04-15 09:07:58 -0300

    fear: past card and home

---
+++ layout2/Home.js
@@ -0,0 +1,17 @@
+"use client"
+import React from "react"
+
+export default function Home() {
+    return (
+        <div className={styles.header}>
+            <div className={styles.image}>
+                <img src="./eu.jpg"></img>
+                <div className={styles.p}>
+                    <p className={styles.pheader}>
+
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
