e57124589c889c0ef5a2220e25e2bb74403917ab
Beatriz L <158210831+limabea23@users.noreply.github.com>
2025-04-15 11:46:33 -0300

    feat: add footer component & add icon

--- beatriz/TabScreen.js
+++ beatriz/TabScreen.js
@@ -2,6 +2,10 @@ import React from "react";
 import { SafeAreaView, View, Text, TextInput, StyleSheet } from "react-native";
 import Card1 from "./components/Card1";
 import Card2 from "./components/Card2";
+import Footer from "./components/Footer";
+
+import Octicons from '@expo/vector-icons/Octicons';
+
 
 export default function TabScreen() {
     return (
@@ -9,10 +13,14 @@ export default function TabScreen() {
             <Text style={styles.title}>Harry Styles</Text>
             <Card1 image={require('./assets/')} />
             <Card1 image={require('./assets/')} />
-            <TextInput style={styles.input} placeholder="O que você deseja ouvir hoje?" />
+            <View style={styles.input}>
+                <TextInput style={styles.input} placeholder="O que você deseja ouvir hoje?" />
+                <Octicons name="dot" size={24} color="black" />
+            </View>
             <Card2 image={require('./assets/')} text="Harry Styles é o primeiro álbum de estúdio do cantor britânico Harry Styles, lançado em 2017. O álbum conta com dez faixas, incluindo o single Sign of the Times."/>
             <Card2 image={require('./assets/')} text="Fine Line é o segundo álbum de estúdio do cantor britânico Harry Styles, lançado em 2019. O álbum é descrito como pop rock, com elementos de outros gêneros."/>
             <Card2 image={require('./assets/')} text="Harry's House é o terceiro álbum de estúdio do cantor britânico Harry Styles, lançado em 2022. O álbum conta com 13 faixas. A canção As It Was foi a mais bem sucedida do álbum."/>
+            <Footer />
         </SafeAreaView>
-    )
+    );
 }
